5a1fd9e2930ab89afa02b2abaa57e3bbf8531f9d
ACCESA\mihaela.meze <mihaela.meze@accesa.eu>
2023-01-27 17:28:49 +0200

    cleaning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AngularFireDatabase,
-  AngularFireList,
-} from '@angular/fire/compat/database';
-import { Router } from '@angular/router';
 import { getDatabase, onValue, ref, remove } from 'firebase/database';
-import { apiConfig, unsplash } from 'src/app/config';
+import { apiConfig } from 'src/app/config';
 import { UnsplashService } from 'src/app/services/unsplash.service';
 import { WeatherService } from 'src/app/services/weather.service';
 import { RegisterService } from '../services/register.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -19,7 +15,6 @@ export class HomeComponent implements OnInit {
   unitSystem: string = '';
 
   WeatherData: any;
-  wind: number;
   icon: string;
   description: string;
   photoUrl: string;
@@ -37,19 +32,16 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private auth: RegisterService,
-    public firedb: AngularFireDatabase,
-    private router: Router,
     private weatherService: WeatherService,
     private unsplashService: UnsplashService
   ) {
     this.isLoggedIn = localStorage.getItem('user');
-    
+
     this.user = this.auth.getUserLoggedIn();
     const db = getDatabase();
     const starRef = ref(db, 'favorites/' + this.user);
 
     onValue(starRef, snapshot => {
-      
       this.dataFromFavorites = snapshot.val();
       Object.values(this.dataFromFavorites).map((data: string) => {
         if (this.favoriteCity.includes(data) === false) {
@@ -61,12 +53,10 @@ export class HomeComponent implements OnInit {
       if (this.favoriteCity.length !== 0) {
         this.getWeatherData(this.cityName);
       }
-      console.log(this.favoriteCity);
     });
   }
 
   ngOnInit(): void {
-
     this.unitSystem = this.weatherService.getUnitSystem();
     const measurementUnits =
       this.unitSystem === 'metric'
@@ -75,13 +65,11 @@ export class HomeComponent implements OnInit {
 
     this.measureOfTemp = measurementUnits.temperature;
 
-    this.weatherService.selectedCityListener().subscribe(
-      selectedCity => {
-        this.getWeatherData(selectedCity);
-      }
-    )
-    
-    if(!this.isLoggedIn) {
+    this.weatherService.selectedCityListener().subscribe(selectedCity => {
+      this.getWeatherData(selectedCity);
+    });
+
+    if (!this.isLoggedIn) {
       this.getWeatherData('Bucharest');
     }
   }
@@ -90,93 +78,73 @@ export class HomeComponent implements OnInit {
     this.weatherService.updateUnitSystem(unitSystem);
   }
 
-  getWeatherData(cityName: any) {
+  async getWeatherData(cityName: any) {
     this.nameToShow = cityName;
     this.cityName = cityName;
     if (this.favoriteCity) {
       this.isAdded = this.favoriteCity.includes(this.nameToShow);
     }
 
-    fetch(
+    await fetch(
       'https://api.openweathermap.org/data/2.5/weather?q=' +
         cityName +
         '&appid=5fe302f14d5bd84b4b60562300f00762'
     )
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        this.setWeatherData(data) });
+        this.setWeatherData(data);
+      });
   }
 
   setWeatherData(data: any) {
     this.WeatherData = data;
-    this.WeatherData.humidity = data.main.humidity;
-    this.wind = data.wind.speed;
     this.icon =
       'http://openweathermap.org/img/wn/' +
       this.WeatherData.weather[0].icon +
       '@2x.png';
-    this.description = this.WeatherData.weather[0].description;
+    this.description = this.WeatherData?.weather[0]?.description;
 
     if (this.unitSystem === 'metric') {
-      this.WeatherData.currentTemperature = (data.main.temp - 273.15).toFixed(
-        0
-      );
-      this.WeatherData.minTemperature = (data.main.temp_min - 273.15).toFixed(
-        0
-      );
-      this.WeatherData.maxTemperature = (data.main.temp_max - 273.15).toFixed(
-        0
-      );
-      this.WeatherData.humidity = data.main.humidity;
-      this.WeatherData.wind = data.wind.speed;
+      this.WeatherData.currentTemperature = this.convertKtoC(data.main.temp);
+      this.WeatherData.minTemperature = this.convertKtoC(data.main.temp_min);
+      this.WeatherData.maxTemperature = this.convertKtoC(data.main.temp_max);
     } else {
-      this.WeatherData.currentTemperature = (
-        (data.main.temp - 273.15) * (9 / 5) +
-        32
-      ).toFixed(0);
-      this.WeatherData.minTemperature = (
-        (data.main.temp_max - 273.15) * (9 / 5) +
-        32
-      ).toFixed(0);
-      this.WeatherData.maxTemperature = (
-        (data.main.temp_min - 273.15) * (9 / 5) +
-        32
-      ).toFixed(0);
-      this.WeatherData.wind = data.wind.speed;
+      this.WeatherData.currentTemperature = this.convertKtoF(data.main.temp);
+      this.WeatherData.minTemperature = this.convertKtoF(data.main.temp_max);
+      this.WeatherData.maxTemperature = this.convertKtoF(data.main.temp_min);
     }
 
     this.photoUrl = this.unsplashService.getImage(
       this.nameToShow.toLowerCase()
     );
-    console.log(this.photoUrl);
     fetch(this.photoUrl)
       .then(photos => photos.json())
-      .then(data => (this.cityImage = data.results[0].urls.regular));
-  }
-
-  onLoadCityList() {
-    this.router.navigate(['/city-list']);
+      .then(data => (this.cityImage = data?.results[0].urls.regular));
   }
 
   addFavorite() {
-    this.auth.createFavorite(this.cityName);
+    this.weatherService.createFavorite(this.cityName);
     this.cityName = this.favoriteCity[this.favoriteCity.length - 1];
-    console.log(this.cityName);
     if (this.favoriteCity) {
-      console.log(this.favoriteCity[this.favoriteCity.length - 1]);
       this.getWeatherData(this.favoriteCity[this.favoriteCity.length - 1]);
     }
   }
 
   removeFavorite() {
     const db = getDatabase();
-    console.log(this.dataFromFavorites);
     this.key = Object.keys(this.dataFromFavorites).find(
       key => (this.dataFromFavorites as any)[key] === this.cityName
     );
     remove(ref(db, 'favorites/' + this.user + '/' + this.key));
     this.favoriteCity.filter(value => value !== this.key);
     window.location.reload();
+  }
+
+  convertKtoC(temp: number): string {
+    return (temp - 273.15).toFixed(0);
+  }
+
+  convertKtoF(temp: number): string {
+    return ((temp - 273.15) * (9 / 5) + 32).toFixed(0);
   }
 }
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,55 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {
-  AngularFireDatabase,
-  AngularFireList,
-} from '@angular/fire/compat/database';
-import { ref, remove } from '@angular/fire/database';
 import { Router } from '@angular/router';
-import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterService {
-  favoritesRef!: AngularFireList<object>;
-  user!: any;
-  dataFromFavorites!: object;
-  favoriteCity: Array<string> = [];
-
-  constructor(
-    private fireauth: AngularFireAuth,
-    private router: Router,
-    public firedb: AngularFireDatabase
-  ) {
-    this.user = this.getUserLoggedIn();
-    this.favoritesRef = firedb.list(`favorites/${this?.user}`);
-    console.log(this.favoritesRef);
-  }
+  constructor(private fireauth: AngularFireAuth, private router: Router) {}
 
   getUserLoggedIn() {
     return localStorage.getItem('user');
-  }
-
-  getTodos() {
-    return this.favoritesRef
-      .snapshotChanges()
-      .pipe(
-        map(changes =>
-          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-        )
-      );
-  }
-
-  createFavorite(fav: any) {
-    console.log(this.favoriteCity.includes(fav));
-    if (this.favoriteCity.includes(fav)) {
-    }
-    return this.favoritesRef.push(fav);
-  }
-
-  removeFavorite(key: string) {
-    return this.favoritesRef.remove(key);
   }
 
   registerUser(email: string, password: string) {
